8d5f8b141e7b09f748ce34baefb335e666947304
fgc17 <fgcoelho1709@outlook.com>
2025-09-30 22:28:06 -0300

    refactor (web): remove unused comments

--- packages/web/source.config.ts
+++ packages/web/source.config.ts
@@ -5,8 +5,6 @@ import {
 	metaSchema,
 } from "fumadocs-mdx/config";
 
-// You can customise Zod schemas for frontmatter and `meta.json` here
-// see https://fumadocs.dev/docs/mdx/collections#define-docs
 export const docs = defineDocs({
 	docs: {
 		schema: frontmatterSchema,
