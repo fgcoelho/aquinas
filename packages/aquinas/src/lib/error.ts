f12090f41992eea3059040343466cd96786cc872
fgcoelho <fernando@bunnyware.com>
2025-10-22 03:51:01 -0300

    refactor (aquinas): improve internal ioc error message display

--- packages/aquinas/src/lib/error.ts
+++ packages/aquinas/src/lib/error.ts
@@ -1,6 +1,17 @@
 export class AquinasError extends Error {
-	constructor(message: string) {
-		super(message);
+	constructor(message: string, originalError?: Error | string) {
+		const originalErrorMessage = originalError
+			? typeof originalError === "string"
+				? originalError
+				: originalError.message
+			: 'no additional info';
+
+		const finalMessage = originalErrorMessage
+			? `${message} - ${originalErrorMessage}`
+			: message;
+
+		super(finalMessage);
+
 		this.name = "AquinasError";
 	}
 }
