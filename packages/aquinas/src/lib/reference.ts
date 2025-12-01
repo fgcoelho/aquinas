7f3e0c35b8c94af757c23528eb812b1b5ba04870
fgcoelho <fernando@bunnyware.com>
2025-12-01 00:08:10 -0300

    refactor(reference): enhance Reference class to support generic name types

--- packages/aquinas/src/lib/reference.ts
+++ packages/aquinas/src/lib/reference.ts
@@ -1,21 +1,32 @@
-class Reference<T> {
+class Reference<T, N extends string = string> {
 	id: symbol;
 	__type: T;
 
 	toJSON: () => string;
 
-	constructor(public readonly name: string) {
+	constructor(readonly name: N) {
 		this.id = Symbol.for(name);
 		this.toJSON = () => name;
 	}
+
+	type<U>(): Reference<U, N> {
+		return new Reference<U, N>(this.name);
+	}
 }
+
+interface ReferenceFn {
+	<T>(name: string): Reference<T>;
+	create<const N extends string>(name: N): Reference<unknown, N>;
+}
+
+const reference: ReferenceFn = (<T>(name: string): Reference<T> =>
+	new Reference<T>(name)) as ReferenceFn;
+
+reference.create = <const N extends string>(name: N): Reference<unknown, N> =>
+	new Reference<unknown, N>(name);
 
 function derivedReference<T>(base: string, ref: Reference<any>) {
-	return new Reference<T>(`${base}:${ref.toJSON()}`);
-}
-
-function reference<T>(name: string): Reference<T> {
-	return new Reference<T>(name);
+	return new Reference<T>(`${base}:${ref.toJSON()}` as string);
 }
 
 function isReference(obj: any): obj is Reference<any> {
